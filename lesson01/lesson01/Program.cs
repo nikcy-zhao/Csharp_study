f73225d4baa6692f11090ccc70d742fb36edd40b
赵越 <zhaoy@kitten.com>
2023-07-25 22:43:13 +0800

    修复一个bug

--- lesson01/lesson01/Program.cs
+++ lesson01/lesson01/Program.cs
@@ -6,7 +6,6 @@ class Program
     {
         int a = 5, b = 10;
         Console.WriteLine("Hello, World! , {0}", a + b);
-        Console.WriteLine("this is a test");
 
         Console.WriteLine("this is a dev, this is master branch");
         Console.WriteLine("this is a dev , this is new dev");
